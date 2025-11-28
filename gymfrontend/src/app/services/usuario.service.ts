5b1d56e465ef9cc1038bebe98720d2c0f83b53c0
MendozaOsco <josemendozaosco@gmail.com>
2025-11-28 11:57:54 -0500

    Fix import path for environment in usuario.service.ts
    Updated the import path for the environment file to use a relative path.

--- gymfrontend/src/app/services/usuario.service.ts
+++ gymfrontend/src/app/services/usuario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment'; // ⬅️ PASO CLAVE: Importar el entorno
+import { environment } from './../../environments/environment'; // ⬅️ ¡CORRECCIÓN FINAL! Usamos ruta RELATIVA
 
 @Injectable({ providedIn: 'root' })
 export class UsuarioService {
