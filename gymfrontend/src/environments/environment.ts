91ad2a0939e35204ea5c9830bbd06161c6e2fc38
MendozaOsco <josemendozaosco@gmail.com>
2025-11-28 12:54:42 -0500

    Update environment.ts

--- gymfrontend/src/environments/environment.ts
+++ gymfrontend/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // CORRECCIÓN: El host y puerto, terminando en barra.
-  // Asumimos que tu backend escucha en el puerto 8080.
+  // Configuración local, asumiendo que tu backend corre en 8080.
   apiUrl: 'http://localhost:8080/', 
 };
